c8a49e785740e6813d7b47fa4c1074ccf550ac6e
cofob <c@cofob.ru>
2021-12-06 13:35:43 +0700

    Added more hashes

--- hashtools/hashtools.py
+++ hashtools/hashtools.py
@@ -14,3 +14,18 @@ class HashTools(commands.Cog):
     async def sha256(self, ctx, *, text: str):
         """SHA256 hash"""
         await ctx.send('SHA256 hash:\n```\n'+hashlib.sha256(text.encode()).hexdigest()+'\n```')
+
+    @commands.command()
+    async def sha512(self, ctx, *, text: str):
+        """SHA512 hash"""
+        await ctx.send('SHA512 hash:\n```\n'+hashlib.sha512(text.encode()).hexdigest()+'\n```')
+
+    @commands.command()
+    async def sha1(self, ctx, *, text: str):
+        """SHA1 hash"""
+        await ctx.send('SHA1 hash:\n```\n'+hashlib.sha1(text.encode()).hexdigest()+'\n```')
+
+    @commands.command()
+    async def md5(self, ctx, *, text: str):
+        """MD5 hash"""
+        await ctx.send('MD5 hash:\n```\n'+hashlib.md5(text.encode()).hexdigest()+'\n```')
